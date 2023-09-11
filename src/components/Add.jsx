380d5613b0f7aeb13c3365da7052e03c36081e04
doochukbeni <doochukbeni@gmail.com>
2023-09-12 03:04:34 +0400

    remove unused import

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -10,96 +10,93 @@ import {
   TextField,
   Tooltip,
   Typography,
-} from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import React, { useState } from 'react';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import React, { useState } from "react";
+
 import {
   DateRange,
   EmojiEmotions,
   Image,
   PersonAdd,
   VideoCameraBack,
-} from '@mui/icons-material';
-import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import ImageIcon from '@mui/icons-material/Image';
+} from "@mui/icons-material";
 
 const Add = () => {
   const [open, setOpen] = useState(false);
 
   const StyledModal = styled(Modal)({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   });
   const UserBox = styled(Box)({
-    display: 'flex',
-    alignItems: 'center',
-    gap: '20px',
-    marginBottom: '20px',
+    display: "flex",
+    alignItems: "center",
+    gap: "20px",
+    marginBottom: "20px",
   });
   return (
     <>
       <Tooltip
         onClick={(e) => setOpen(true)}
-        title='Add'
+        title="Add"
         sx={{
-          position: 'fixed',
+          position: "fixed",
           bottom: 20,
-          left: { xs: 'calc(50% - 25)', md: 30 },
+          left: { xs: "calc(50% - 25)", md: 30 },
         }}
       >
-        <Fab color='primary' aria-label='add'>
+        <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
       </Tooltip>
       <StyledModal
         open={open}
         onClose={(e) => setOpen(false)}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
         <Box
           width={400}
           height={280}
-          bgcolor={'background.default'}
-          color={'text.primary'}
+          bgcolor={"background.default"}
+          color={"text.primary"}
           p={3}
           borderRadius={5}
         >
-          <Typography variant='h6' color='gray' textAlign='center'>
+          <Typography variant="h6" color="gray" textAlign="center">
             Create Post
           </Typography>
           <UserBox>
             <Avatar
               sx={{ width: 30, height: 30 }}
-              src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
             />
-            <Typography variant='h6'>Roi Doo</Typography>
+            <Typography variant="h6">Roi Doo</Typography>
           </UserBox>
           <TextField
-            sx={{ width: '100%' }}
-            id='standard-multiline-static'
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
             multiline
             rows={3}
             placeholder="what's on your mind"
-            variant='standard'
+            variant="standard"
           />
-          <Stack direction='row' gap={1} mt={3}>
-            <EmojiEmotions color='primary' />
-            <Image color='secondary' />
-            <VideoCameraBack color='success' />
-            <PersonAdd color='error' />
+          <Stack direction="row" gap={1} mt={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
           </Stack>
           <ButtonGroup
             fullWidth
-            sx={{ marginTop: '10px' }}
-            variant='contained'
-            aria-label='outlined primary button group'
+            sx={{ marginTop: "10px" }}
+            variant="contained"
+            aria-label="outlined primary button group"
           >
             <Button>Post</Button>
-            <Button sx={{ width: '100px' }}>
+            <Button sx={{ width: "100px" }}>
               <DateRange />
             </Button>
           </ButtonGroup>
